9b86ba403071272342c9bf58a316d7e3b6f4d580
Paul Riviere <priviere@z4r1p2.le-101.fr>
2019-10-17 12:57:30 +0200

    corrected trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,44 +6,53 @@
 /*   By: priviere <priviere@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/09 12:36:44 by priviere     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/16 18:51:52 by priviere    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/17 11:46:19 by priviere    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int check_beginning(char c, const char *set)
+{
+	int j;
+
+	j = 0;
+	while (set[j] != '\0')
+	{
+		if (set[j] == c)
+			return (1);
+		j++;
+	}
+	return (0);
+}
+
+int check_end(char c, const char *set)
+{
+	int j;
+
+	j = 0;
+	while (set[j] != '\0')
+	{
+		if (set[j] == c)
+			return (1);
+		j++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	int     start;
     int     len;
-	int 	i;
-	int		j;
 
-	i = 0;
-    if (s1[0] == set[0])
-	{
-    	while (s1[i] == set[i] && set[i] != '\0')
-			i++;
-		if (set[i] == '\0')
-			start = ft_strlen(set);
-		else
-			start = 0;
-	}
-	j = 0;
-	while (s1[j])
-	{
-		if (s1[j] == set[0])
-		{
-            i = 0;
-			while (set[i] == s1[j + i] && set[i] != '\0')
-				i++;
-			if (s1 [j + i] == '\0' && set[i] == '\0')
-				len = ft_strlen(s1) - start - ft_strlen(set);
-			else
-				len = ft_strlen(s1) - start;
-		}
-		j++;
-	}
-    return(ft_substr(s1, start, len));
+	start = 0;
+	len = ft_strlen(s1);
+	while (check_beginning(s1[start], set))
+		start++;
+	while (check_end(s1[len - 1], set))
+		len--;
+	if (len - start <= 0)
+		return ("");
+    return(ft_substr(s1, start, (len - start)));
 }
